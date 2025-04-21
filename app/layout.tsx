adeed97700a1c02572d37321a61e16e0a3ea7e47
georgeokez <georgeokez@gmail.com>
2025-04-21 12:15:33 +0100

    feat: change logo and add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Quick Drive',
   description: 'A lightweight, highly customizable online drive with a friendly user interface. Quick Drive provides a modern and efficient way to manage your files in the cloud',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-96x96.png', sizes: '96x96' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
